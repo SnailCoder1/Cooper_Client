57ea6a1be49ba4f876d38c7d6a6f87b76d4a1228
Boa Matule <boa.matule@gmail.com>
2019-10-03 17:16:05 +0200

    Fills up the form with imputs for the password.

--- src/Components/SignUpForm.jsx
+++ src/Components/SignUpForm.jsx
@@ -1,9 +1,32 @@
 import React from "react"
+import { Form, Button } from "semantic-ui-react"
 
 const SignUpForm = (props) => {
   return (
     <Form id="sign-up-form">
-      
+      <Form.Imput
+      id="email"
+      placeholder="Email"
+      onchange={props.inputChangeHandler}
+      />
+
+    <Form.Imput
+      type="password"
+      id="password"
+      placeholder="Password"
+      onchange={props.inputChangeHandler}
+      />
+
+    <Form.Imput
+      type="password"
+      id="passwordConfirmation"
+      placeholder="passwordConfirmation"
+      onchange={props.inputChangeHandler}
+      />
+
+      <Button compact color="teal" onClick={(e) => props.signUpHandler(e)} id="submit">Submit</Button>
+      <Button compact color="teal" onClick={(e) => props.resetHandler(e)}>Reset</Button>
+    
     </Form>
   )
 }
