944ec9b133c0e254e4582dcae954658a3e2bab44
Boa Matule <boa.matule@gmail.com>
2019-10-10 16:18:40 +0200

    Fix typo errors to allow the sign up component to render. WIP

--- src/Components/SignUpForm.jsx
+++ src/Components/SignUpForm.jsx
@@ -4,14 +4,14 @@ import { Form, Button } from "semantic-ui-react"
 const SignUpForm = (props) => {
   return (
     <Form id="sign-up-form">
-      <Form.Imput
+      <Form.Input
 			fluid
       id="email"
       placeholder="Email"
       onchange={props.inputChangeHandler}
       />
 
-      <Form.Imput
+      <Form.Input
         fluid
         type="password"
         id="password"
@@ -19,7 +19,7 @@ const SignUpForm = (props) => {
         onchange={props.inputChangeHandler}
       />
 
-      <Form.Imput
+      <Form.Input
         fluid
         type="password"
         id="passwordConfirmation"
@@ -27,8 +27,8 @@ const SignUpForm = (props) => {
         onchange={props.inputChangeHandler}
       />
 
-      <Button compact color="teal" onClick={(e) => props.signUpHandler(e)} id="submit">Submit</Button>
-      <Button compact color="teal" onClick={(e) => props.resetHandler(e)}>Reset</Button>
+      <Button compact onClick={(e) => props.signUpHandler(e)} id="submit">Submit</Button>
+      <Button compact onClick={(e) => props.resetHandler(e)}>Reset</Button>
     
     </Form>
   )
